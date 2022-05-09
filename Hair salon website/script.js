a0c916dbe14ae9c58fe1c164f0e02802f1cbfc3e
shubhamsigdar <shubhamsigdar@gmail.com>
2022-05-09 05:36:34 +0530

    sample website

--- Hair salon website/script.js
+++ Hair salon website/script.js
@@ -9,11 +9,12 @@ const navSlide =()=>{
 
             //Animate Links
         navLinks.forEach((link, index)=>{
-            if(link.style.animation){
-              link.style.animation=''
-           }else{
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
-           }
+           //  if(link.style.animation){
+           //    link.style.animation=''
+           // }else{
+              link.style.animation = `navLinkFade 2s ease forwards ${index/7 +0.5 }s`;
+           // }
+          console.log(index);
         });
         //Burger animation
         burger.classList.toggle('toggle');
